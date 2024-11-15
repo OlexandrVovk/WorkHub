0170027c13dbb7324dfd8c2a0e292c073dd5d7c7
olexandr <sashawowk222@gmail.com>
2024-11-15 14:12:55 +0200

    adds new empty module - "task-service"

---
+++ task-service/src/main/java/com/code_galacticos/taskservice/TaskServiceApplication.java
@@ -0,0 +1,13 @@
+package com.code_galacticos.taskservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskServiceApplication.class, args);
+    }
+
+}
