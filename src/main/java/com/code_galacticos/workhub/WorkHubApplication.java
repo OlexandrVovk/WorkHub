5e60e4b7a30b819d7c35ef2009375fab51f5159e
olexandr <sashawowk222@gmail.com>
2024-11-15 12:46:39 +0200

    first commit

---
+++ src/main/java/com/code_galacticos/workhub/WorkHubApplication.java
@@ -0,0 +1,13 @@
+package com.code_galacticos.workhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkHubApplication.class, args);
+    }
+
+}
