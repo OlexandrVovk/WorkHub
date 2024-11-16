4277f493fcce908cc6f232a7d315fc1d8f9031ec
olexandr <sashawowk222@gmail.com>
2024-11-16 15:43:13 +0200

    fix lombok error

--- notification-service/src/main/java/com/code_galacticos/notificationservice/controller/EmailController.java
+++ notification-service/src/main/java/com/code_galacticos/notificationservice/controller/EmailController.java
@@ -3,24 +3,33 @@ package com.code_galacticos.notificationservice.controller;
 import com.code_galacticos.notificationservice.model.dto.EmailRequest;
 import com.code_galacticos.notificationservice.service.EmailService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/notifications")
-@RequiredArgsConstructor
 public class EmailController {
 
     private final EmailService emailService;
 
-    @PostMapping("/email")
-    public ResponseEntity<String> sendEmail(@RequestBody EmailRequest request) {
-        emailService.sendEmail(request.getTo(), "Hello World", "Take yours' POVISTKA");
-        return ResponseEntity.ok("Email sent successfully");
+    @Autowired
+    public EmailController(EmailService emailService) {
+        this.emailService = emailService;
     }
 
     @GetMapping
-    public void sayHi() {
-        System.out.println("hi");
+    public ResponseEntity<String> sayHi() {
+        return ResponseEntity.ok("Email Service is working!");
+    }
+
+    @PostMapping("/email")
+    public ResponseEntity<String> sendEmail(@RequestBody EmailRequest request) {
+        emailService.sendEmail(
+                request.getTo(),
+                request.getSubject(),
+                request.getText()
+        );
+        return ResponseEntity.ok("Email sent successfully");
     }
 }
--- notification-service/src/main/java/com/code_galacticos/notificationservice/service/EmailService.java
+++ notification-service/src/main/java/com/code_galacticos/notificationservice/service/EmailService.java
@@ -1,16 +1,20 @@
 package com.code_galacticos.notificationservice.service;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class EmailService {
 
     private final JavaMailSender emailSender;
+
+    @Autowired
+    public EmailService(JavaMailSender emailSender) {
+        this.emailSender = emailSender;
+    }
 
     @Value("${notification.email.sender}")
     private String sender;
